6fa4cde47b9e24de04f91b781f9fd77818800ac2
JhonChino <jchino.181ci12@istta.edu.pe>
2021-11-18 08:13:19 -0500

    rutas web

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/clinica/listar');
 });
+Route::get('/clinica', 'App\Http\Controllers\TestController@crearClinica')->name('clinica.agregar');
+Route::post('/clinica', 'App\Http\Controllers\TestController@registrarClinica')->name('clinica.guardar');
+Route::get('/clinica/listar', 'App\Http\Controllers\TestController@listarClinica')->name('clinica.listar');
+Route::get('/clinica/editar/{clinica}', 'App\Http\Controllers\TestController@formularioEditarClinica')->name('clinica.editar');
+Route::patch('/clinica/editar/{clinica}', 'App\Http\Controllers\TestController@ActualizarClinica')->name('clinica.actualizar');
+Route::get('/clinica/{clinica}', 'App\Http\Controllers\TestController@VerClinica')->name('clinica.ver');
+Route::delete('/clinica/{clinica}', 'App\Http\Controllers\TestController@EliminarClinica')->name('clinica.eliminar');
